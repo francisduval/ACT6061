9542a3c7e3d497f619bdbfb9c8f2128bbce978ed
Francis Duval <francisduval37@gmail.com>
2023-02-13 14:27:15 -0500

    Corrections

--- Exercice_5_11.R
+++ Exercice_5_11.R
@@ -1,12 +1,30 @@
 library(here)
 
+# Importer le jeu de données. On peut utiliser un « path » relatif si on utilise un projet R (fichier .Rproj) ===================
 freaggnumber <- read.table(here("data", "freaggnumber.txt"), sep = ";") 
 
-fit_poisson <- glm(ClaimNumber ~ DriverAge + LicenceAge + VehAge + Exposure, family = poisson(link = "log"), data = freaggnumber)
-fit_quasipoisson <- glm(ClaimNumber ~ DriverAge + LicenceAge + VehAge + Exposure, family = quasipoisson(link = "log"), data = freaggnumber)
 
+# Ajuster la régression Poiusson et la régression quasi-Poisson =================================================================
+fit_poisson <- 
+  glm(
+    formula = ClaimNumber ~ DriverAge + LicenceAge + VehAge, 
+    family = poisson(link = "log"), 
+    data = freaggnumber,
+    offset = log(Exposure)
+  )
+
+fit_quasipoisson <- 
+  glm(
+    formula = ClaimNumber ~ DriverAge + LicenceAge + VehAge, 
+    family = quasipoisson(link = "log"), 
+    data = freaggnumber,
+    offset = log(Exposure)
+  )
+
+
+# Observer l'ajustement avec summary() et les AIC avec AIC() ====================================================================
 summary(fit_poisson)
 summary(fit_quasipoisson)
 
-AIC(fit_poisson) # 71790.79
+AIC(fit_poisson) # 51758.91
 AIC(fit_quasipoisson) # NA
